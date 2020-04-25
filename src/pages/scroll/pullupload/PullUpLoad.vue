05ad71047766fbd5368af2df2a4c1d58e4e58d07
lizhen0211 <lizhen0211@163.com>
2020-04-25 12:57:59 +0800

    滚动组件

--- src/pages/scroll/pullupload/PullUpLoad.vue
+++ src/pages/scroll/pullupload/PullUpLoad.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="container">
-        <div ref="pullupscroll" class="pullup-scroll-wrapper">
+        <div ref="pullupscroll" class="pullup-scroll-bswrapper">
             <div class="pullup-scroll-content">
                 <ul>
-                    <li v-for="item in this.items">{{item}}</li>
+                    <li v-for="item in this.items" class="pullup-item">{{item}}</li>
                 </ul>
+                <div class="pullup-bottom-wrapper">
+                    <div v-if="isShowLoading">
+                        <span class="pullup-txt">Loading...</span>
+                    </div>
+                    <div v-if="isLoadFinish">
+                        <span class="pullup-txt">noting to load</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>

097b043d29cf2f608b3d963365124086856af167
lizhen <lizhen0211@163.com>
2020-06-02 09:13:58 +0800

    jsbridge

--- src/pages/jsbridge/otherJsBridge/other-jsbridge.ts
+++ src/pages/jsbridge/otherJsBridge/other-jsbridge.ts
@@ -7,6 +7,8 @@ export default class OtherJsBridge extends Vue {
         this.connectWebViewJavascriptBridge(function (bridge: any) {
             console.log("connectWebViewJavascriptBridge");
             bridge.init(function (message: string, responseCallback: Function) {
+                console.log("bridge.init callback")
+                //native 调用 js
                 console.log('JS got a message', message);
                 var data = {
                     'Javascript Responds': '测试中文!'
@@ -18,9 +20,11 @@ export default class OtherJsBridge extends Vue {
                 }
             });
 
+            //注册native 调用 js 的回调方法
             bridge.registerHandler("functionInJs", function (data: string, responseCallback: Function) {
                 // @ts-ignore
                 // document.getElementById("show").innerHTML = ("data from Java: = " + data);
+                console.log("bridge.registerHandler callback")
                 console.log("data from Java: = " + data);
                 if (responseCallback) {
                     console.log("call registerHandler responseCallback");
@@ -54,6 +58,8 @@ export default class OtherJsBridge extends Vue {
         // @ts-ignore
         window.WebViewJavascriptBridge.send(data
             , function (responseData: string) {
+                console.log("window.WebViewJavascriptBridge.send callback")
+                //js 的回调函数
                 console.log("repsonseData from java, data = " + responseData);
             }
         );
@@ -66,6 +72,8 @@ export default class OtherJsBridge extends Vue {
             'submitFromWeb'
             , {'param': '中文测试'}
             , function(responseData:string) {
+                //js 的回调函数
+                console.log("window.WebViewJavascriptBridge.callHandler callback")
                 console.log("send get responseData from java, data = " + responseData)
             }
         );
--- src/pages/lifecycle/vuelifecycle.ts
+++ src/pages/lifecycle/vuelifecycle.ts
@@ -1,5 +1,5 @@
 import {Component, Vue} from "vue-property-decorator";
-
+//文档地址：https://cn.vuejs.org/v2/api/#%E9%80%89%E9%A1%B9-%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F%E9%92%A9%E5%AD%90
 @Component
 export default class VueLifecycle extends Vue {
 
@@ -32,7 +32,7 @@ export default class VueLifecycle extends Vue {
     }
 
     activated() {
-        console.log("updated")
+        console.log("activated")
     }
 
     deactivated() {
